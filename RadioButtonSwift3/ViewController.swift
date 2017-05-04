b8ce87697d9bce0b3c20fe73ef24423a76b2839e
Abdullah Alhazmy <me@alhazmy13.net>
2017-05-04 10:54:16 +0300

    Initial Commit

---
+++ RadioButtonSwift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RadioButtonSwift3
+//
+//  Created by Abdullah Alhazmy on 5/4/17.
+//  Copyright © 2017 Abdullah Alhazmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
